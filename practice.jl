7fc50a982e58dc6a8b7d811d4a90a6f490e4a599
MarsFenix23 <48728570+MarsFenix23@users.noreply.github.com>
2019-03-19 14:53:19 -0400

    Add files via upload

---
+++ practice.jl
@@ -0,0 +1,13 @@
+# WRITTEN IN THE REPL
+# ] add IJulia InstantiateFromURL # Enter package
+# mode and install link to Jupyter and manager for
+# QE dependencies.
+
+# QuantEcon packages
+# using InstantiateFromURL
+# activate_github("QuantEcon/QuantEconLectureAllPackages", tag = "v0.9.6", add_default_environment = true)
+
+# Status and Gadfly plotting package
+# ]
+#status
+# add Gadfly
